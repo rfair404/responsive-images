f64fd05e2e4d95c3b028d014001e0772441d6842
scneptune <rmalcome@gmail.com>
2014-03-11 12:21:21 -0700

    changed some sizes around and added a few new ones. also added some option defaults.
    Signed-off-by: scneptune <rmalcome@gmail.com>

--- lib/common.php
+++ lib/common.php
@@ -7,14 +7,14 @@ class Common{
     public $version, $slug, $js_namespace, $settings, $lib_url;
 
     function __construct(){
-        $this->version = 001;
+        $this->version = 002;
         $this->slug = 'responsive-image-upsizer';
         $this->js_namespace = 'ResponsiveImageUpsizer';
         $this->settings = get_option($this->slug);
         $this->lib_url = plugins_url( '/lib', dirname(__FILE__) );
-
+        $this->default_selector = $this->slug;
         add_filter( $this->slug . '-sizes' , array( $this, 'default_sizes' ) );
-
+        add_action('init', array($this, 'init_check'));
     }
 
     /**
@@ -27,13 +27,21 @@ class Common{
     function default_sizes( $sizes = array() ){
         $default_sizes = array(
             'responsive-base' => array( 'width' => 80, ),
-            'responsive-size1' => array( 'width' => 320, ),
-            'responsive-size2' => array( 'width' => 640, ),
-            'responsive-size3' => array( 'width' => 980, ),
-            'responsive-size4' => array( 'width' => 1600, ),
+            'responsive-size1' => array( 'width' => 120, ),
+            'responsive-size2' => array( 'width' => 140, ),
+            'responsive-size3' => array( 'width' => 320, ),
+            'responsive-size4' => array( 'width' => 640, ),
+            'responsive-size5' => array( 'width' => 980, ),
+            'responsive-size6' => array( 'width' => 1600, ),
         );
         return wp_parse_args( $sizes, $default_sizes );
     }
 
+    function init_check() {
+        if (get_option('responsive_images_option') == false ) {
+            add_option('selector_name', $this->slug, null, 'no');
+            add_option('selector_mask', '', null, 'no');
+        }
+    }
 
 }
